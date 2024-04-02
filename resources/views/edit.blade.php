1f65c10caf92886c0b10921b2968f6790d38d2e4
Joseph Parra <josephpt2004@outlook.com>
2024-04-01 22:38:18 -0500

    Departamento model

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -8,37 +8,51 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Add comuna</title>
-  </head>
-  <body>
-  <div class="container">
-    <h1>Add comuna</h1>
-    <form method= "POST" action="{{ route('comunas.store')}}">
-        @csrf
-  <div class="mb-3">
-    <label for="id" class="form-label">Code</label>
-    <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id" disabled="disabled">
-    <div id="idHelp" class="form-text">Commune Code</div>
-  </div>
-  <div class="mb-3">
-    <label for="name" class="form-label">Commune</label>
-    <input type="text" required class="form-control" id="name" aria-describedby="nameHelp" name="name" 
-    placeholder="Comuna name.">
-  </div>
-  <label for="municipality">Municipality</label>
-    <select class="form-select" id="municipality" name="code" required>
-    <option selected disabled value="" > Choose one...</option>
-    @foreach ($municipios as $municipio)
-    <option value="{{ $municipio->muni_codi}}">{{$municipio->muni_nomb}}</option>
-    @endforeach
-</select>
-<div class="mb-3">
-  <button type="submit" class="btn btn-primary">Save</button>
-  <a href="{{ route('comunas.index')}}" class="btn btn-warning">Cancel</a>
-  </div>
-</form>
-</div>
+    <title>Edit Comuna</title>
 
+  </head>
+
+  <body>
+    <div class="container">
+      <h1>Edit Comuna</h1>
+       <form method="POST" action="{{ route('comunas.update', ['comuna' => $comuna->comu_codi]) }}">
+        @method('put')
+        @csrf
+        <div class="mb-3">
+          <label for="codigo" class="form-label">Id</label>
+          <input type="text" class="form-control" id="id" aria-describedby="codigoHelp" name="id"
+          disabled="disabled" value="{{ $comuna->comu_codi }}">
+          <div id="codigoHelp" class="form-text">Commune Id.</div>
+        </div>
+
+        <div class="mb-3">
+          <label for="name" class="form-label">Commune</label>
+          <input type="text" required class="form-control" id="name" aria-describedby="Commune name"
+             name="name" value="{{ $comuna->comu_nomb }}">
+        </div>
+
+        <label for="municipality">Municipality:</label>
+        <select class="form-select"  id="municipality" name="code" required>
+            <option selected disabled value="">Choose one...</option>
+
+            @foreach ($municipios as $municipio)
+
+            @if ($municipio->muni_codi == $comuna->muni_codi)
+               <option  selected value="{{$municipio->muni_codi}}">{{$municipio->muni_nomb}}</option>
+
+            @else
+               <option value="{{$municipio->muni_codi}}">{{$municipio->muni_nomb}}</option>
+
+            @endif
+
+            @endforeach
+        </select>
+        <div class="mt-3">
+            <button type="submit" class="btn btn-primary">Update</button>
+            <a href="{{ route('comunas.index') }}" class="btn btn-warning">Cancel</a>
+        </div>
+      </form>
+    </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
